9e7c94ae563c77d8ffef0883f826d7676ab1bdb4
cmhfahim <cmhfahim@gmail.com>
2024-06-06 16:40:30 +0600

    Update CG.py

--- CG.py
+++ CG.py
@@ -77,12 +77,12 @@ def calculate(num1, num2, operation):
     elif operation == 'Divide':
         return num1 / num2
 
-st.title('Simple Calculator')
+stl.title('Simple Calculator')
 
-num1 = st.number_input('Enter first number')
-num2 = st.number_input('Enter second number')
-operation = st.selectbox('Choose an operation', ['Add', 'Subtract', 'Multiply', 'Divide'])
+num1 = stl.number_input('Enter first number')
+num2 = stl.number_input('Enter second number')
+operation = stl.selectbox('Choose an operation', ['Add', 'Subtract', 'Multiply', 'Divide'])
 
-if st.button('Calculate'):
+if stl.button('Calculate'):
     result = calculate(num1, num2, operation)
     st.write('The result is ', result)
