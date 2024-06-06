a23a3a44c35758921150baf3f0f5ad089d149625
cmhfahim <cmhfahim@gmail.com>
2024-06-06 16:37:58 +0600

    Update CG.py

--- CG.py
+++ CG.py
@@ -66,3 +66,27 @@ if stl.button("See payments Roadmap"):
 
         elif see=="3rd Installment":
                 stl.write("Third Installment Amount :",res[3])
+
+
+
+import streamlit as st
+
+def calculate(num1, num2, operation):
+    if operation == 'Add':
+        return num1 + num2
+    elif operation == 'Subtract':
+        return num1 - num2
+    elif operation == 'Multiply':
+        return num1 * num2
+    elif operation == 'Divide':
+        return num1 / num2
+
+st.title('Simple Calculator')
+
+num1 = st.number_input('Enter first number')
+num2 = st.number_input('Enter second number')
+operation = st.selectbox('Choose an operation', ['Add', 'Subtract', 'Multiply', 'Divide'])
+
+if st.button('Calculate'):
+    result = calculate(num1, num2, operation)
+    st.write('The result is ', result)
