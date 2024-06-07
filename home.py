d3f31b12b70993c21aaad2a92c832c03a942fd1d
cmhfahim <cmhfahim@gmail.com>
2024-06-07 11:41:54 +0600

    Add files via upload

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as stl
+import requests
+from streamlit_lottie import st_lottie
+import json
+def home():
+    def url(url):
+        req=requests.get(url)
+        if req.status_code !=200:
+            return None
+        return req.json()
+    ur=url("https://lottie.host/607ecaf0-fac1-4de8-af38-4f57ca4fef7c/A818GRE2Xt.json")
+    st_lottie(ur,height=300,key="Education")
+    stl.markdown("<h3 style='text-align: center; color: #E7D2CC;'>This is to solve some common problem of UIU students</h3>", unsafe_allow_html=True)
