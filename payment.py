103e9a4e9efd375699979bb00bc231f713a8025b
cmhfahim <cmhfahim@gmail.com>
2025-07-20 20:17:03 +0600

    Update payment.py

--- payment.py
+++ payment.py
@@ -17,63 +17,60 @@ def payment():
             remain_amount=0
             tution_fee=(5525*credit)
 
-            if waiver=="0% Waiver":
+            if waiver=="0% Waiver or Scholarship":
                 remain_amount=100
-            elif waiver=="25% Waiver":
+            elif waiver=="25% Waiver or Scholarship":
                 remain_amount=75
-            elif waiver=="50% Waiver":
+            elif waiver=="50% Waiver or Scholarship":
                 remain_amount=50
-            elif waiver=="100% Waiver":
+            elif waiver=="100% Waiver or Scholarship":
                 remain_amount=0
 
             discount_total_fee_1=((tution_fee*(remain_amount/100)))+6500
             if waiver=="100% Waiver" :
                 return discount_total_fee_1
             else:
-                reg_pay=15000
-
-                discount_total_fee=discount_total_fee_1-reg_pay
-                installment_1=discount_total_fee*(40/100)
+                
+                installment_1=discount_total_fee_1*(40/100)
                 reamin_fee=discount_total_fee-installment_1
                 installment_2=reamin_fee/2
                 reamin_fee-=installment_2
                 installment_3=installment_2
-                lst=[discount_total_fee_1,reg_pay,installment_1,installment_2,installment_3]
+                lst=[discount_total_fee_1,installment_1,installment_2,installment_3]
                 return lst
 
         
 
-    credit=stl.number_input("Enter Your Total Credits: ")
+    credit_1=stl.number_input("Enter Your Total Credits (Non-Retake): ")
+    credit_2=stl.number_input("Enter Your Total Credits (Retake): ")
 
+    credit=(credit_1+(credit_2)/2)
     waiver = stl.selectbox('Please Provide Necessery Information', ['0% Waiver', '25% Waiver', '50% Waiver', '100% Waiver'])
 
     see = stl.selectbox('Enter your Choice', ['See Payment Roadmap', '1st Installment', '2nd Installment', '3rd Installment'])
 
     if stl.button("See payments Roadmap"):
         res=fee(credit,waiver)
-        if waiver=="100% Waiver":
+        if waiver=="100% Waiver or Scholarship":
             if see=="See Payment Roadmap":
                     stl.write("You Have to Pay Total:",res)
-                    stl.write("You Have to Pay at registration :",15000)
                     stl.write("Remain balance will be adjusted")
             else:
                     stl.write("You don't need to pay in this installment")
             
         else:
             if see=="See Payment Roadmap":
-
                     stl.write("You Have to Pay Total:",res[0])
-                    stl.write("You Have to Pay at registration :",res[1])
-                    stl.write(" Pay your First Installment before: Jun 25, 2024:",res[2])
-                    stl.write("Pay your Second Installment before: Jul 16, 2024:",res[3])
-                    stl.write("Pay your Third Installment before: Aug 14, 2024:",res[4])
+                    stl.write(" Pay your First Installment before: August 13, 2025:",res[1])
+                    stl.write("Pay your Second Installment before: September 14, 2025:",res[2])
+                    stl.write("Pay your Third Installment before: October 12, 2025:",res[3])
 
             elif see=="1st Installment":
 
-                    stl.write(" Pay your First Installment before: Jun 25, 2024:",res[2])
+                    stl.write(" Pay your First Installment before: August 13, 2025:",res[1])
 
             elif see=="2nd Installment":
-                    stl.write("Pay your Second Installment before: Jul 16, 2024:",res[3])
+                    stl.write("Pay your Second Installment before: September 14, 2025:",res[2])
 
             elif see=="3rd Installment":
-                    stl.write("Pay your Third Installment before: Aug 14, 2024:",res[4])
+                    stl.write("Pay your Third Installment before: October 12, 2025:",res[3])
