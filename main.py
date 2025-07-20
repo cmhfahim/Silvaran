f967963359711c8ea31a39e37a512125633cef80
cmhfahim <cmhfahim@gmail.com>
2025-07-20 19:49:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import streamlit as stl
 from home import home
 from CG import CGPA
 from payment import payment
-from PIL import Image
-import requests
-from streamlit_lottie import st_lottie
 from feedback import feedback
 
 stl.markdown("<h1 style='text-align: center; color: Black;'>SILVARAN</h1>", unsafe_allow_html=True)
 
+if "page" not in stl.session_state:
+    stl.session_state.page = "Homepage"  
+
+
 stl.sidebar.title("Navigation")
+
 if stl.sidebar.button("Homepage"):
-    home()
+    stl.session_state.page = "Homepage"
 if stl.sidebar.button("Payment Roadmap"):
-    payment()
+    stl.session_state.page = "Payment"
 if stl.sidebar.button("CGPA Calculator"):
-    CGPA()
+    stl.session_state.page = "CGPA"
 if stl.sidebar.button("Feedback"):
+    stl.session_state.page = "Feedback"
+
+# Show the selected page
+if stl.session_state.page == "Homepage":
+    home()
+elif stl.session_state.page == "Payment":
+    payment()
+elif stl.session_state.page == "CGPA":
+    CGPA()
+elif stl.session_state.page == "Feedback":
     feedback()
